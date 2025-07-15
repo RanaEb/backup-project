6e6fc11971bf35674e04f30840c943b540a8ad4e
Reyhaneh <reyhaneh.eb46@gmail.com>
2025-07-15 20:15:25 +0330

    Add: handle -h/--help, unknown parameters, and close case and while loop

--- backup.sh
+++ backup.sh
@@ -15,3 +15,14 @@ while [[ "$#" -gt 0 ]]; do
     -r|--retention) RETENTION_DAYS="$2"; shift ;;
     --dry-run) DRY_RUN=true ;;
     --encrypt) ENCRYPT=true ;;
+    -h|--help)
+      echo "Usage: $0 -d <backup_dir> -f <extensions> [-r <retention_days>] [--dry-run] [--encrypt]"
+      exit 0
+      ;;
+    *)
+      echo "Unknown parameter: $1"
+      exit 1
+      ;;
+  esac
+  shift
+done
