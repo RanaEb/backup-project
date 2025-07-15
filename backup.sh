948f04b27102ae72a4622f8e584eefe354a4852b
Reyhaneh <reyhaneh.eb46@gmail.com>
2025-07-15 20:12:13 +0330

    Add: define config, log, retention, and dry-run variables

--- backup.sh
+++ backup.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 # Automated Backup Script
 # Usage: ./backup.sh -d <backup_directory> -f <formats> [-r <retention_days>] [--dry-run] [--encrypt]
+CONFIG_FILE="backup.conf"
+LOG_FILE="backup.log"
+RETENTION_DAYS=7
+DRY_RUN=false
